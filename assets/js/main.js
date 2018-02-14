2b508d8e8f39eb1b8696c7a27d28478083267fed
Diego Ballesteros <diballesterosc@gmail.com>
2018-02-14 16:40:55 -0500

    Format
    -Formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var weather = document.getElementById("weather");
+    var weather = document.getElementById("weather");
 
-var location = document.getElementById("location"); 
+    var location = document.getElementById("location");
 
-var icon = document.getElementById("icon");
+    var icon = document.getElementById("icon");
 
-var temperature;
+    var temperature;
 
-/* Get Geolocation from the navigator */
-function getWeather() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showInfo);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    /* Get Geolocation from the navigator */
+    function getWeather() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showInfo);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
     }
-}
 
-/* Call the FCC weather API, get the temperature, area, country and icon that describes the weather and insert
-into respective HTML*/
-function showInfo(position) {
+    /* Call the FCC weather API, get the temperature, area, country and icon that describes the weather and insert
+    into respective HTML*/
+    function showInfo(position) {
 
-    var call = "https://fcc-weather-api.glitch.me/api/current?lat=" + Math.round(position.coords.latitude) + 
-    "&lon=" + Math.round(position.coords.longitude); 
+        var call = "https://fcc-weather-api.glitch.me/api/current?lat=" + Math.round(position.coords.latitude) +
+            "&lon=" + Math.round(position.coords.longitude);
 
-    $.getJSON(call,function(json){
-        console.log(json.main.temp);
-        temperature =json.main.temp;
-        location.innerHTML = json.name + ", " + json.sys.country;
-        weather.innerHTML = temperature;
-        icon.innerHTML = "<img src=" + json.weather[0].icon + ">";
+        $.getJSON(call, function (json) {
+            console.log(json.main.temp);
+            temperature = json.main.temp;
+            location.innerHTML = json.name + ", " + json.sys.country;
+            weather.innerHTML = temperature;
+            icon.innerHTML = "<img src=" + json.weather[0].icon + ">";
+        });
+
+    }
+
+    /* Upon clicking the scale (Celsius/Fahrenheit) change to other and vice versa */
+    $(document).on('click', "#scale", function () {
+        if ('C' == $(this).text()) {
+            weather.innerHTML = celsiusToFahrenheit(temperature);
+            scale.innerHTML = "F";
+        }
+        else if ('F' == $(this).text()) {
+            weather.innerHTML = fahrenheitToCelius(temperature);
+            scale.innerHTML = "C";
+        }
     });
 
-}
-
-/* Upon clicking the scale (Celsius/Fahrenheit) change to other and vice versa */
-$(document).on('click', "#scale", function () {
-    if('C' == $(this).text()){
-        weather.innerHTML = celsiusToFahrenheit(temperature);
-        scale.innerHTML ="F"; 
+    /* Conversions */
+    function celsiusToFahrenheit(c) {
+        temperature = c * 1.8 + 32;
+        return temperature;
     }
-    else if('F' == $(this).text()){
-        weather.innerHTML = fahrenheitToCelius(temperature);
-        scale.innerHTML ="C"; 
+
+    function fahrenheitToCelius(f) {
+        temperature = Math.round((f - 32) * .5556)
+        return temperature;
     }
-});
 
-/* Conversions */
-function celsiusToFahrenheit(c){
-    temperature = c * 1.8 + 32;
-    return temperature;
-} 
-
-function fahrenheitToCelius (f){
-    temperature = Math.round((f-32) * .5556)
-    return temperature;
-}
-
-// Call the API upon loading the page
-getWeather();
+    // Call the API upon loading the page
+    getWeather();
 
 });
